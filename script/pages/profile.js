56f9f18bd8094d2d70a8d550918d10b80c497e45
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2022-12-29 11:03:56 +0530

    responsive changes

---
+++ script/pages/profile.js
@@ -0,0 +1,17 @@
+let filter_block = document.getElementsByClassName('filter-block');
+
+let openAdvanceFilter = (e) => {
+ if (window.innerWidth < 991) {
+  if (e.target.closest('.sidebar').classList.contains('opened')) {
+   e.target.closest('.sidebar').classList.remove('opened')
+  }
+  else {
+   e.target.closest('.sidebar').classList.add('opened')
+  }
+ }
+}
+
+
+if (filter_block) {
+ filter_block[0].addEventListener("click", openAdvanceFilter)
+}
